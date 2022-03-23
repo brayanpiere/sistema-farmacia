bb63e2da499d2c77efc012434dccb0c988b6c425
Moises Melendez Hualpa <moise.melendez@hotmail.com>
2022-03-22 23:24:29 -0500

    Agregar logica a la ruta realizar
    Agregando consulta sql para mostrar en la view de realizar, mas otros cambios en estilo

--- controllers/compras.js
+++ controllers/compras.js
@@ -1,9 +1,19 @@
+// Invocando a la base de datos
+const BD = require('../config/config');
+
 const renderIndex = (req, res) => {
     res.render("compras/index");
 }
 
-const renderRealizar = (req, res) => {
-    res.render("compras/realizar");
+const renderRealizar = async (req, res) => {
+    const sql = 'select p."nombreProducto", p."composicion", pre."nombrePresentacion" \
+    from "Producto" p, "Presentacion" pre \
+    WHERE p."idPresentacion" = pre."idPresentacion"'
+
+    const { rows } = await BD.open(sql)
+    // Rows forma parte del result de la consulta sql
+    console.log(rows);
+    res.render("compras/realizar", { prods: rows });
 }
 
 module.exports = { renderIndex, renderRealizar }
