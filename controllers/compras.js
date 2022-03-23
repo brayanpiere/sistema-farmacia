1e5f60d58284acf6a02e3fe0280a7f7b91a849fd
Moises Melendez Hualpa <moise.melendez@hotmail.com>
2022-03-23 00:25:13 -0500

    Mejorando la consulta sql de la ruta realizar compra

--- controllers/compras.js
+++ controllers/compras.js
@@ -6,13 +6,16 @@ const renderIndex = (req, res) => {
 }
 
 const renderRealizar = async (req, res) => {
-    const sql = 'select p."nombreProducto", p."composicion", pre."nombrePresentacion" \
-    from "Producto" p, "Presentacion" pre \
-    WHERE p."idPresentacion" = pre."idPresentacion"'
+    const sql = 'select p."nombreProducto", p."composicion", pre."nombrePresentacion", lot."stock" \
+    from "Producto" p, "Presentacion" pre, "LoteProducto" lot \
+    where p."idPresentacion" = pre."idPresentacion" \
+    and lot."idProducto" = p."idProducto" \
+    and lot."estado" = \'Activo\''
 
+    // rows forma parte del result de la consulta sql
+    // const result = await BD.open(sql)
     const { rows } = await BD.open(sql)
-    // Rows forma parte del result de la consulta sql
-    console.log(rows);
+    // console.log(rows);
     res.render("compras/realizar", { prods: rows });
 }
 
