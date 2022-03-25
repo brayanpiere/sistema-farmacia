c21b66883ccb76ff0814f3c35270ae15ab536654
Moises Melendez Hualpa <moise.melendez@hotmail.com>
2022-03-25 17:44:36 -0500

    Ayuda a Brayan 1
    Recuperar los childnodes con la informacion de los campos de la tabla de cargos

--- assets/js/cargo.js
+++ assets/js/cargo.js
@@ -2,6 +2,10 @@ const cargarModalAddCargo = () => {
     modalAddCargo.toggle();
 }
 
+const mostrarValores = (e) => {
+    console.log(e.target.parentNode.parentNode.childNodes[5].textContent.trim())
+}
+
 const main = () => {
 
     const divModalAddCargo = document.querySelector("#modal_addCargo");
@@ -10,7 +14,13 @@ const main = () => {
     const btnAddCargo = document.querySelector("#btn-addCargo");
     btnAddCargo.addEventListener("click", cargarModalAddCargo);
 
-    
+    const btnModificar = document.querySelectorAll(".modificar");
+    // console.log(btnModificar);
+    for (const but of btnModificar) {
+        but.addEventListener("click", mostrarValores)
+    }
+
+    const btnEliminar = document.querySelector(".eliminar");
 }
 
 window.addEventListener("load", main);
